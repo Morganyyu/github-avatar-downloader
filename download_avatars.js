3dbff49c1c68225ff2d1120c4c2314ad2de72a51
Morganyyu <morganyu120@hotmail.com>
2018-03-20 01:17:14 +0000

    modified download_avatars.js and added secret.js to .gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,20 @@
 var request = require('request');
+var token = require('./secrets')
 // var baseUrl = 'https://api.github.com/repos/jquery/jquery/contributors';
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
 
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
