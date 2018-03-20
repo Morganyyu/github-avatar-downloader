b44f7d622c8dffe39a77aa559e6a517acbff370d
Morganyyu <morganyu120@hotmail.com>
2018-03-20 00:50:30 +0000

    added getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,13 @@
 var request = require('request');
+// var baseUrl = 'https://api.github.com/repos/jquery/jquery/contributors';
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
